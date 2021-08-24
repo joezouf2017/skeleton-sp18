03e173c4d512595e9dad1a052541cd6937a1d97e
Joe Zou <tzou@berkeley.edu>
2021-08-24 12:33:11 +0800

    add new case for TOBO

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -2,7 +2,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestOffByOne {
-
     // You must use this CharacterComparator and not instantiate
     // new ones, or the autograder might be upset.
     static CharacterComparator offByOne = new OffByOne();
@@ -15,5 +14,9 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('a', 'z'));
         assertFalse(offByOne.equalChars('a', 'a'));
         assertFalse(offByOne.equalChars('a', 'd'));
+        assertTrue(offByOne.equalChars('&',  '%'));
+        assertFalse(offByOne.equalChars('&', '&'));
+        assertTrue(offByOne.equalChars('A',  'B'));
+        assertFalse(offByOne.equalChars('A',  'A'));
     }
 }
