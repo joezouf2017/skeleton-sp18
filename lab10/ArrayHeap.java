533ceebb20100921e976c370a1202ed405a25628
Joe Zou <tzou@berkeley.edu>
2022-01-18 20:05:00 +0800

    Update ver2 removeMin

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -169,7 +169,9 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
         swap(1, size);
         size--;
         contents[size + 1] = null;
-        sink(1);
+        if (size!= 0) {
+            sink(1);
+        }
         return item;
     }
 
