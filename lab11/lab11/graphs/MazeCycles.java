8fa4fda86a60f1317cc3d6d2fd499a1298b119d2
Joe Zou <tzou@berkeley.edu>
2022-02-05 23:41:56 +0800

    Finish Cycles

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,47 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s = 0;
+    private boolean hasCycle;
+    private int[] track;
+
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        distTo[s] = 0;
+        track = new int[m.V()];
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(s);
     }
 
     // Helper methods go here
+    public void dfs(int v) {
+        marked[v] = true;
+        announce();
+        for (int nbr : maze.adj(v)) {
+            if (hasCycle) {
+                return;
+            } else if (marked[nbr] && distTo[nbr] != distTo[v] - 1) {
+                edgeTo[nbr] = v;
+                hasCycle = true;
+                int cur = v;
+                while (cur != nbr) {
+                    edgeTo[cur] = track[cur];
+                    cur = track[cur];
+                }
+                announce();
+            } else if (!marked[nbr]) {
+                marked[nbr] = true;
+                announce();
+                distTo[nbr] = distTo[v] + 1;
+                track[nbr] = v;
+                dfs(nbr);
+            }
+        }
+    }
 }
 
